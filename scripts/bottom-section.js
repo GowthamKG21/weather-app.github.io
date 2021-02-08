cde66c10e2d09027ab89593107e35ad2990714df
GowthamKG21 <GowthamKG21@users.noreply.github.com>
2021-02-08 16:33:29 +0530

    feat: js: add EventListeners for sort Icons
    Signed-off-by: GowthamKG21 <GowthamKG21@users.noreply.github.com>

--- scripts/bottom-section.js
+++ scripts/bottom-section.js
@@ -5,4 +5,21 @@ import { initializeJSON } from './initialize-json.js';
 
 let jsonData = initializeJSON();
 jsonData.then(function (data) {
+  // DOM elements for Sort Arrow Icons
+  let sortByContinent = document.getElementById("sortContinent");
+  sortByContinent.addEventListener("click", () => { sortCity("Cont"); });
+
+  let SortByContinentArrow = document.getElementById("sortContinentImg");
+  SortByContinentArrow.src = "/./assets/icons/general-icons/arrowDown.svg";
+  SortByContinentArrow.alt = "Down Arrow Icon";
+
+  let sortByTemperature = document.getElementById("sortTemperature");
+  sortByTemperature.addEventListener("click", () => { sortCity("Temp"); });
+
+  let SortByTemperatureArrow = document.getElementById("sortTemperatureImg");
+  SortByTemperatureArrow.src = "/./assets/icons/general-icons/arrowDown.svg";
+  SortByTemperatureArrow.alt = "Down Arrow Icon";
+
+  // Bottom City Card Container
+  let bContainer = document.getElementById("bottomContainer");
 })
